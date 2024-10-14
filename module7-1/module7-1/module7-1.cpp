5f9e3876b4bda72de7bf2b6cca9c16cdd9720e75
Your Namenikita2-2 <nvoskovcev@gmail.com>
2024-10-14 19:45:33 +0300

    first commit

---
+++ module7-1/module7-1/module7-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <memory>
+
+void printArray(const std::unique_ptr<int[]>& arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Element " << i << ": " << arr[i] << std::endl;
+    }
+}
+
+int main() {
+    const int size = 10;
+    std::unique_ptr<int[]> arr = std::make_unique<int[]>(size);
+
+    std::cout << "Enter " << size << " numbers: ";
+    for (int i = 0; i < size; ++i) {
+        std::cin >> arr[i];
+    }
+
+    printArray(arr, size);
+
+    return 0;
+}
